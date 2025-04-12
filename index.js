b2c42aa6ed75c0bc725fb9e8a6782cacd014a468
Rei0003 <rei03lus@gmail.com>
2025-04-12 22:55:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-function updateClock(){
-
+function Clock(){
   const now = new Date();
+  const checkbox = document.getElementById("check");
   let hours = now.getHours();
-  const meridiem = hours >= 12 ? "PM" : "AM";
-  hours = hours % 12 || 12
-  hours = hours.toString().padStart(2, 0);
   const minutes = now.getMinutes().toString().padStart(2, 0);
   const seconds = now.getSeconds().toString().padStart(2, 0);
-  const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
-  document.getElementById("clock").textContent = timeString;
+
+
+  if(checkbox.checked){
+    const meridiem = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12
+    hours = hours.toString().padStart(2, 0);
+    const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
+    document.getElementById("clock").textContent = timeString;
+  }
+  else{
+    hours = hours.toString().padStart(2, '0'); 
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    document.getElementById("clock").textContent = timeString;
+  }
+}
+function displayDate(){
+  const now = new Date();
+  const year = now.getFullYear().toString();
+  const month = (now.getMonth() + 1).toString().padStart(2, 0);
+  const day = now.getDate().toString().padStart(2, 0);
+  const dateString = `${day}.${month}.${year}`
+  document.getElementById("date").textContent = dateString
 }
 
-updateClock();
-setInterval(updateClock, 1000);
+Clock();
+displayDate();
+setInterval(Clock, 1000)
+setInterval(displayDate, 86400000)
